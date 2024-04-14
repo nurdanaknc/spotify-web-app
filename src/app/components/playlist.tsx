878745d67528878484b352e78a8d16bb4101eb79
nurdanaknc <nurdan.akinci@ogr.sakarya.edu.tr>
2024-04-15 00:53:49 +0300

    tracks listed (style fixed)

--- src/app/components/playlist.tsx
+++ src/app/components/playlist.tsx
@@ -14,19 +14,19 @@ export default function Playlist(props: any) {
   const [playlist, setPlaylist] = useState<any>([]);
 
 
-  const darkenColor = (color:any, darkenAmount:any) => {
-      // Renk bileşenlerini ayırma
-      const red = parseInt(color.slice(1, 3), 16);
-      const green = parseInt(color.slice(3, 5), 16);
-      const blue = parseInt(color.slice(5, 7), 16);
+  const darkenColor = (color: any, darkenAmount: any) => {
+    // Renk bileşenlerini ayırma
+    const red = parseInt(color.slice(1, 3), 16);
+    const green = parseInt(color.slice(3, 5), 16);
+    const blue = parseInt(color.slice(5, 7), 16);
 
-      // Her bir renk bileşenine bir miktar koyuluk ekleme
-      const newRed = Math.max(0, red - darkenAmount);
-      const newGreen = Math.max(0, green - darkenAmount);
-      const newBlue = Math.max(0, blue - darkenAmount);
+    // Her bir renk bileşenine bir miktar koyuluk ekleme
+    const newRed = Math.max(0, red - darkenAmount);
+    const newGreen = Math.max(0, green - darkenAmount);
+    const newBlue = Math.max(0, blue - darkenAmount);
 
-      // Yeni renk oluşturma
-      return `#${newRed.toString(16).padStart(2, '0')}${newGreen.toString(16).padStart(2, '0')}${newBlue.toString(16).padStart(2, '0')}`;
+    // Yeni renk oluşturma
+    return `#${newRed.toString(16).padStart(2, '0')}${newGreen.toString(16).padStart(2, '0')}${newBlue.toString(16).padStart(2, '0')}`;
   };
 
 
@@ -51,44 +51,56 @@ export default function Playlist(props: any) {
     console.log(playlist, "playlist");
   }, [selectedPlaylist]);
 
-  useEffect(() => { 
+  useEffect(() => {
     console.log(gradient, "gradient")
   }
-  , [gradient])
+    , [gradient])
 
 
-return (
-    <div className={`flex flex-col gap-8 w-screen rounded-2xl`} style={{background: gradient }}>
-        <div>
-            {playlist && (
-                <div className={`flex flex-col  px-3 py-2 ` } >
-                   <div className="flex flex-row mt-16 gap-4  items-end "> 
-                    {playlist.images && playlist.images[0] && <img src={playlist.images[0].url} alt="playlist" className=" rounded w-44 h-44 shadow-black shadow-2xl drop-shadow-xl" />}
-                    <div className="flex flex-col gap-4">
-                        <span className=" text-xs text-gray-500">
-                            {playlist.type?.charAt(0).toUpperCase() + playlist.type?.slice(1)}
-                        </span>
-                        <span className="font-bold text-6xl">  {playlist.name?.charAt(0).toUpperCase() + playlist.name?.slice(1)}</span>
-                        <span className="text-sm font-semibold text-gray-500">
-                            {playlist.owner?.display_name} &bull; {playlist.tracks?.total} songs
-                        </span>
-                    </div>
-                    </div>
+  return (
+    <div className={`flex flex-col gap-8 w-screen rounded-2xl`} style={{ background: gradient }}>
+      <div>
+        {playlist && (
+          <div className={`flex flex-col  px-3 py-2 `} >
+            <div className="flex flex-row mt-16 gap-4  items-end ">
+              {playlist.images && playlist.images[0] && <img src={playlist.images[0].url} alt="playlist" className=" rounded w-44 h-44 shadow-black shadow-2xl drop-shadow-xl" />}
+              <div className="flex flex-col gap-4">
+                <span className=" text-xs text-gray-500">
+                  {playlist.type?.charAt(0).toUpperCase() + playlist.type?.slice(1)}
+                </span>
+                <span className="font-bold text-6xl">  {playlist.name?.charAt(0).toUpperCase() + playlist.name?.slice(1)}</span>
+                <span className="text-sm font-semibold text-gray-500">
+                  {playlist.owner?.display_name} &bull; {playlist.tracks?.total} songs
+                </span>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className=" bg-grey6 bg-transparent backdrop-blur-3xl  h-full bg-opacity-20 p-3">
+        {playlist.tracks?.items && (
+          <ol className="mt-8 flex  flex-col  gap-4">
+            {playlist.tracks.items.map((track: any, index: number) => (
+              <li key={index} className="flex ">
+                <div className="flex flex-row gap-4 items-center">
+                  <span >{index + 1}</span>
+                  <img
+                    src={track?.track.album.images[0].url}
+                    alt="album"
+                    className="w-12 h-12 rounded"
+                  />
+                  <div className="flex flex-col">
+                    <span>{track?.track.name}</span>
+                    <span className=" text-grey4">
+                      {track?.track.artists.map((artist: any) => artist.name).join(", ")}
+                    </span>
+                  </div>
                 </div>
-            )}
-        </div>
-        <div className=" bg-grey6 bg-transparent backdrop-blur-3xl  h-full bg-opacity-20 p-3">
-              {playlist.tracks?.items && (
-                <ol className="mt-8">
-                  {playlist.tracks.items.map((track: any, index: number) => (
-                    <li key={index} className="flex items-center gap-4">
-                      <span className="text-gray-500">{index + 1}.</span>
-                      <span>{track?.track.name}</span>
-                    </li>
-                  ))}
-                </ol>
-              )}
-        </div>
+              </li>
+            ))}
+          </ol>
+        )}
+      </div>
     </div>
-);
+  );
 }
